ec2791f2f1d78cdc586eb5bde953c7ad5cfa8ff2
sterdsterd <sterdsterd@icloud.com>
2019-03-10 01:29:05 +0900

    Detect Most Accurate Activity

--- app/src/main/java/net/sterdsterd/walker/ActivityIntentService.java
+++ app/src/main/java/net/sterdsterd/walker/ActivityIntentService.java
@@ -11,10 +11,9 @@ import android.content.res.Resources;
 import com.google.gson.reflect.TypeToken;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
-//Extend IntentService//
+
 public class ActivityIntentService extends IntentService {
     protected static final String TAG = "Activity";
-    //Call the super IntentService constructor with the name for the worker thread//
     public ActivityIntentService() {
         super(TAG);
     }
@@ -22,17 +21,12 @@ public class ActivityIntentService extends IntentService {
     public void onCreate() {
         super.onCreate();
     }
-//Define an onHandleIntent() method, which will be called whenever an activity detection update is available//
-
     @Override
     protected void onHandleIntent(Intent intent) {
-//Check whether the Intent contains activity recognition data//
         if (ActivityRecognitionResult.hasResult(intent)) {
 
-//If data is available, then extract the ActivityRecognitionResult from the Intent//
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
 
-//Get an array of DetectedActivity objects//
             ArrayList<DetectedActivity> detectedActivities = (ArrayList) result.getProbableActivities();
             PreferenceManager.getDefaultSharedPreferences(this)
                     .edit()
@@ -40,9 +34,18 @@ public class ActivityIntentService extends IntentService {
                             detectedActivitiesToJson(detectedActivities))
                     .apply();
 
+            DetectedActivity mostProbableActivity
+                    = result.getMostProbableActivity();
+            int activityTypeCode = mostProbableActivity.getType();
+            String activityType = getActivityString(this, activityTypeCode);
+            PreferenceManager.getDefaultSharedPreferences(this)
+                    .edit()
+                    .putString(net.sterdsterd.walker.MainActivityKt.ACTIVITY_TYPE,
+                            activityType)
+                    .apply();
+
         }
     }
-//Convert the code for the detected activity type, into the corresponding string//
 
     static String getActivityString(Context context, int detectedActivityType) {
         Resources resources = context.getResources();
--- app/src/main/java/net/sterdsterd/walker/MainActivity.kt
+++ app/src/main/java/net/sterdsterd/walker/MainActivity.kt
@@ -1,7 +1,6 @@
 package net.sterdsterd.walker
 
 import android.app.Activity
-import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -11,24 +10,19 @@ import android.view.MenuItem
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.location.ActivityRecognitionClient
-import com.google.android.gms.location.ActivityRecognitionResult
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import android.preference.PreferenceManager
-import com.google.android.gms.location.DetectedActivity
 import android.app.PendingIntent
 import android.content.Intent
 import android.view.View
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.android.gms.tasks.Task
 
 
 const val DETECTED_ACTIVITY = ".DETECTED_ACTIVITY"
+const val ACTIVITY_TYPE = ".ACTIVITY_TYPE"
 
 class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
-
-    var mContext: Context? = null
 
     var mActivityRecognitionClient: ActivityRecognitionClient? = null
 
@@ -47,7 +41,7 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
 
         fab.setOnClickListener { view ->
             if(isGooglePlayServiceAvailable(this)) {
-                requestUpdatesHandler(centreText)
+                requestUpdatesHandler(recognisedData)
 
             } else Snackbar.make(view, "Not Available", Snackbar.LENGTH_LONG).show()
         }
@@ -68,30 +62,30 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
     }
 
     fun requestUpdatesHandler(view: View) {
-        //Set the activity detection interval. I’m using 3 seconds//
         val task = mActivityRecognitionClient?.requestActivityUpdates(
-            3000,
+            500,
             getActivityDetectionPendingIntent()
         )
         task?.addOnSuccessListener { updateDetectedActivitiesList() }
     }
 
-    //Get a PendingIntent//
     private fun getActivityDetectionPendingIntent(): PendingIntent {
-        //Send the activity data to our DetectedActivitiesIntentService class//
         val intent = Intent(this, ActivityIntentService::class.java)
         return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
     }
 
-    //Process the list of activities//
     protected fun updateDetectedActivitiesList() {
         val detectedActivities = ActivityIntentService.detectedActivitiesFromJson(
             PreferenceManager.getDefaultSharedPreferences(this)
                 .getString(DETECTED_ACTIVITY, "")
         )
-        centreText.setText(detectedActivities.toString())
-        /**mAdapter.updateActivities(detectedActivities)*/
+        recognisedData.setText(detectedActivities.toString())
+
+        val activityType = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(ACTIVITY_TYPE, "")
+        statusText.setText(activityType)
+
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, s: String) {
@@ -101,15 +95,11 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
